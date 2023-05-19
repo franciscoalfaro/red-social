3a9ce38966534f5969c52cf55c6503703cd32f72
franciscoalfar@gmail.com <*Fran2811*>
2023-05-19 01:26:34 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { Header } from './components/layout/private/Header'
+
+
+
+function App() {
+
+
+  return (
+
+    <div class="layout">
+      <Header></Header>
+    </div>
+
+
+
+
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+//import assets 
+import './assets/fonts/fontawesome-free-6.1.2-web/css/all.css';
+import './assets/css/normalize.css';
+import './assets/css/responsive.css';
+import './assets/css/styles.css'
+
+
+
+//arrancar app de react
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    <App />
+
+)
