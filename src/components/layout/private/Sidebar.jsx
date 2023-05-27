d0d2bedc0929762e9876d49e8162463ec3af44f8
franciscoalfar@gmail.com <*Fran2811*>
2023-05-27 16:52:57 -0400

    componente de publicaciones

--- src/components/layout/private/Sidebar.jsx
+++ src/components/layout/private/Sidebar.jsx
@@ -1,12 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import avatar from '../../../assets/img/user.png'
 import useAuth from '../../../hooks/useAuth'
 import { Global } from '../../../helpers/Global'
 import { Link } from 'react-router-dom'
+import { useForm } from '../../../hooks/useForm'
 
 
 export const Sidebar = () => {
-    const {auth, counters} = useAuth()
+    const { auth, counters } = useAuth()
+    const { form, changed } = useForm()
+    const [stored, setStored ] = useState("not_stored")
+
+    const savePublication = async (e) => {
+        e.preventDefault()
+
+        const token = localStorage.getItem('token')
+
+        //recoger datos del formulario
+        let newPublication = form
+        newPublication.user = auth._id
+
+        //hacer requesr
+        const request = await fetch(Global.url + "publication/save", {
+            method: "POST",
+            body: JSON.stringify(newPublication),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            }
+        })
+
+        const data = await request.json()
+
+        //mostrar mejs
+        if (data.status == "success") {
+            setStored("stored")
+        } else {
+            setStored("error")
+        }
+
+        //subir imag
+        const fileInput = document.querySelector("#file")
+
+        if(data.status == "success" && fileInput.files[0]){
+
+            const formaData = new FormData()
+            formaData.append("file0", fileInput.files[0])
+
+            const uploadRequest = await fetch(Global.url+"publication/upload/"+ data.publicationStored._id,{
+                method: "POST",
+                body: formaData,
+                headers: {
+                    "Authorization": token
+                }
+            })
+            const uploadData = await uploadRequest.json()
+            if(uploadData.status == "success"){
+                setStored("stored")
+            }else{
+                setStored("error")
+            }
+
+            if(data.status =="success" && uploadData.status =="success" ){
+                const myForm= document.querySelector("#publication-form")
+                myForm.reset()
+            }
+
+        }
+    }
 
 
     return (
@@ -22,8 +83,8 @@ export const Sidebar = () => {
 
                     <div className="profile-info__general-info">
                         <div className="general-info__container-avatar">
-                            {auth.image =='default.png' && <img src={avatar} className="container-avatar__img" alt="Foto de perfil"></img>}
-                            {auth.image !='default.png' && <img src={Global.url+"user/avatar/"+auth.image} className="container-avatar__img" alt="Foto de perfil"></img>}
+                            {auth.image == 'default.png' && <img src={avatar} className="container-avatar__img" alt="Foto de perfil"></img>}
+                            {auth.image != 'default.png' && <img src={Global.url + "user/avatar/" + auth.image} className="container-avatar__img" alt="Foto de perfil"></img>}
 
                         </div>
 
@@ -36,13 +97,13 @@ export const Sidebar = () => {
 
                     <div className="profile-info__stats">
                         <div className="stats__following">
-                            <Link to={"siguiendo/"+auth._id} className="following__link">
+                            <Link to={"siguiendo/" + auth._id} className="following__link">
                                 <span className="following__title">Siguiendo</span>
                                 <span className="following__number">{counters.following}</span>
                             </Link>
                         </div>
                         <div className="stats__following">
-                            <Link to={"seguidores/"+auth._id} className="following__link">
+                            <Link to={"seguidores/" + auth._id} className="following__link">
                                 <span className="following__title">Seguidores</span>
                                 <span className="following__number">{counters.followed}</span>
                             </Link>
@@ -58,21 +119,19 @@ export const Sidebar = () => {
 
 
                 <div className="aside__container-form">
+                    {stored == "stored" ? <strong className='alert alert-success'>Publicacion realizada correctamente</strong> : ""}
+                    {stored == "error" ? <strong className='alert alert-warning'>error al realizar publicacion intenta nuevamente</strong> : ""}
 
-                    <form className="container-form__form-post">
-
+                    <form id="publication-form" className="container-form__form-post" onSubmit={savePublication}>
                         <div className="form-post__inputs">
-                            <label htmlFor="post" className="form-post__label">¿Que estas pesando hoy?</label>
-                            <textarea name="post" className="form-post__textarea"></textarea>
+                            <label htmlFor="text" className="form-post__label">¿Que estas pesando hoy?</label>
+                            <textarea name="text" className="form-post__textarea" onChange={changed}></textarea>
                         </div>
-
                         <div className="form-post__inputs">
-                            <label htmlFor="image" className="form-post__label">Sube tu foto</label>
-                            <input type="file" name="image" className="form-post__image"></input>
+                            <label htmlFor="file" className="form-post__label">Sube tu foto</label>
+                            <input type="file" name="file0" id="file" className="form-post__image"></input>
                         </div>
-
-                        <input type="submit" value="Enviar" className="form-post__btn-submit" disabled></input>
-
+                        <input type="submit" value="Enviar" className="form-post__btn-submit"></input>
                     </form>
 
                 </div>
