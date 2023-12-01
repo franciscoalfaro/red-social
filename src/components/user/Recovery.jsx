cb372e6c1d0ca5bac013ed8eb5705a1529149f4d
franciscoalfar@gmail.com <*Fran2811*>
2023-11-30 22:33:56 -0300

    mejoras en vista de recover

--- src/components/user/Recovery.jsx
+++ src/components/user/Recovery.jsx
@@ -3,6 +3,8 @@ import { useForm } from '../../hooks/useForm';
 import { Global } from '../../helpers/Global';
 import { Spiner } from '../../hooks/Spiner';
 import { useNavigate } from 'react-router-dom'
+import userImage from '../../assets/img/user.png'
+
 
 
 export const Recovery = () => {
@@ -28,17 +30,18 @@ export const Recovery = () => {
       const data = await request.json();
 
       if (data.status === "success") {
-        // Mostramos un mensaje de éxito usando SweetAlert2
         Swal.fire({
           position: 'center',
           icon: 'success',
-          title: 'En caso de existir se enviará correo con clave provisional',
-          showConfirmButton: false,
-          timer: 1150
+          title: 'En caso de existir cuenta se enviará correo con clave provisional',
+          showConfirmButton: true,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // Redirigir a la página de login
+            navigate('/login');
+          }
         });
-
-        setTimeout(() => { window.location.reload() }, 1200);
-        navigate('/login')
+      
       } else {
         // Mostramos un mensaje de error si la solicitud no fue exitosa
         Swal.fire({
@@ -60,13 +63,16 @@ export const Recovery = () => {
       <div className="row justify-content-center mt-5">
         <div className="col-md-6 login-container">
           <div className="login-form text-center">
-            <h2>Recuperar Clave</h2>
+            <h2>Recupera tu clave</h2>
             <form onSubmit={recoverUser}>
               <div className="form-group">
-                <label htmlFor="email" className="">Dirección de correo</label>
-                <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={changed} required />
+                <img src={userImage} alt="Imagen superior" className="img-fluid mb-3"/>
               </div>
-              {loading ? ( // Mostrar el spinner si loading es true
+              <div className="form-group">
+                <label htmlFor="email" className="">Dirección de correo</label>
+                <input type="email" name="email" className="form-control" id="exampleInputEmail1" placeholder='correo@testsocial.org' aria-describedby="emailHelp" onChange={changed} required />
+              </div>
+              {loading ? (
                 <Spiner />
               ) : (
                 <button type="submit" className="btn btn-primary">Recuperar Clave</button>
@@ -75,6 +81,7 @@ export const Recovery = () => {
           </div>
         </div>
       </div>
+
     </>
   );
 };
